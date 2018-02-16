2c2ae38aac8ea09ade994fe3a8538a39d55bf9e5
Unknown <50kite17050buster130@gmail.com>
2018-02-15 20:48:19 -0600

    Gutted robot.cpp & robot.h
    Gutted robot.cpp & robot.h and started to add things back in

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -4,85 +4,56 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-#include "Robot.h"
-#include "wpilib.h"
 
+#include <Robot.h>
 CORE2018 * Robot = nullptr;
 
 CORE2018::CORE2018() :
-	driveSubsystem()
-	//m_driverJoystick(0),
-	//m_operatorJoystick(1)
-	{
+driveSubsystem(),
+
+m_driverJoystick(0),
+
+m_operatorJoystick(1)
+
+{
 	Robot = this;
 }
-void CORE2018::StartCompetition() {
+CORE2018::~CORE2018() {
+	Robot = nullptr;
 
 }
-
 void CORE2018::RobotInit() {
-
 }
 
- void CORE2018::DisabledInit() {
+void CORE2018::DisabledInit() {
+}
 
- }
-
- CORE2018::~CORE2018() {
- 	Robot = nullptr;
- }
-
- START_ROBOT_CLASS(CORE2018);
-
-
+void CORE2018::TeleopPeriodic() {
+}
 //void CORE2018::robotInit() {
-//
 //}
 //void CORE2018::teleopInit() {
-//
 //}
 //void CORE2018::teleop() {
-//
 //}
-
-
 // void Robot::AutonomousInit() {
-//
+
 // }
-//
 // void Robot::AutonomousPeriodic(){
-//
 // }
-
-
-//
 // void Robot::TeleopInit(){
-//
 // }
-// void Robot::TestInit(){
-//
+// void Robot::TestInit() {
 // }
-//
 // void Robot::RobotPeriodic(){
-//
 // }
-//
 // void Robot::DisabledPeriodic(){
-//
 // }
-//
+
 // void Robot::TeleopPeriodic() {
-//
 // }
-//
 // void Robot::TestPeriodic(){
-//
 // }
-//
 // void Robot::StartCompetition() {
-//
 // }
-//
-
-
-
+START_ROBOT_CLASS(CORE2018)
